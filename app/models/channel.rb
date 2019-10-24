51da0881791eabe46a0249f2cc2daa5255105c21
Brandao Plaster <brandaoplaster@gmail.com>
2019-10-24 09:59:27 -0300

    Validation included for channel fields and relationship

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,4 +1,7 @@
 class Channel < ApplicationRecord
   belongs_to :user
   belongs_to :team
+  has_many :messages, as: :messagable, :dependent => :destroy
+
+  validates_presence_of :slug, :team, :user
 end
