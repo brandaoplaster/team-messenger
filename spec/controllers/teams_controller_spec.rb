60cc6d9077951af78a26d9868fa086f00f957a52
brandaoplaster <brandaoplaster@gmail.com>
2019-11-05 17:28:52 -0300

    Test created for show method

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -18,6 +18,38 @@ RSpec.describe TeamsController, type: :controller do
 
   describe "GET #show" do
 
+    context "Team exists" do
+      it "Returns success" do
+        team = create(:team, user: @current_user)
+        get :show, params: { slug: team.slug }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "User is member of the team" do
+      it "Returns success" do
+        team = create(:team)
+        team.users << @current_user
+        get :show, params: { slug: team.slug }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "User is not the owner or member of the team" do
+      it "Redirect to root" do
+        team = create(:team)
+        get :show, params: { slug: team.slug }
+        expect(response).to redirect_to('/')
+      end
+    end
+
+    context "team don't exists" do
+      it "Redirects to root" do
+        team_attributes = attibutes_for(:team)
+        get :show, params: { slug: team_attributes[:slug] }
+        expect(response).to redirect_to('/')
+      end
+    end
   end
 
   describe "POST #create" do
