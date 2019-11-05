9b1d7a2e5f8873c95e45e43c887fbe672c4c9c29
brandaoplaster <brandaoplaster@gmail.com>
2019-11-05 14:52:28 -0300

    Test created for TeamsController and included configuration with Devise

---
+++ spec/controllers/teams_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe TeamsController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @current_user = FactoryGirl.create(:user)
+    sign_in @current_user
+  end
+
+end
