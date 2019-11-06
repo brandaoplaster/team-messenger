89aed9b779fedcbbb9f84c2c60b3166f828cb240
brandaoplaster <brandaoplaster@gmail.com>
2019-11-06 11:26:15 -0300

    Test created for controller channel create method, in scenario where user is a team member

--- spec/controllers/channels_controller_spec.rb
+++ spec/controllers/channels_controller_spec.rb
@@ -12,7 +12,36 @@ RSpec.describe ChannelsController, type: :controller do
   end
 
   describe "POST #create" do
+    render_views
 
+    context "User is team member" do
+
+      before(:each) do
+        @team = create(:team)
+        @team.users << @current_user
+
+        @channel_attributes = attributes_for(:channel, team: @team, user: @current_user)
+        post :create, params: { channel: @channel_attributes.merge(team_id: @team.id) }
+      end
+
+      it "Returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "Channel is created with right params" do
+        expect(Channel.last.slug).to eql(@channel_attributes[:slug])
+        expect(Channel.last.user).to eql(@current_user)
+        expect(Channel.last.team).to eql(@team)
+      end
+
+      it "Returns right values to channel" do
+        response_hash = JSON.parser(response.body)
+
+        expect(response_hash["user_id"]).to eql(@current_user.id)
+        expect(response_hash["slug"]).to eql(@channel_attributes[:slug])
+        expect(response_hash["team_id"]).to eql(@team.id)
+      end
+    end
   end
 
   describe "GET #show" do
