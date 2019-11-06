c2a4d21d7702182c296ce7ae30905170f03d57e0
brandaoplaster <brandaoplaster@gmail.com>
2019-11-06 10:46:55 -0300

    Created channels controller test and including devise settings to perform tests

---
+++ spec/controllers/channels_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe ChannelsController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  before(:each) do
+    request.env["HTTP_ACCEPT"] = 'application/json'
+
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @current_user = FactorGirl.create(:user)
+    sign_in @current_user
+  end
+
+  describe "POST #create" do
+
+  end
+
+  describe "GET #show" do
+
+  end
+
+  describe "DELETE #destroy" do
+
+  end
+
+end
