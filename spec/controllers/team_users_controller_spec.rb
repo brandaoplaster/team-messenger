9bd4419a50dcff9c77b8f2973a9c875e806261ed
brandaoplaster <brandaoplaster@gmail.com>
2019-11-12 23:29:26 -0300

    Implemented test for the destroy method, in case the team has a leader and in the scenario where a team does have a leader

--- spec/controllers/team_users_controller_spec.rb
+++ spec/controllers/team_users_controller_spec.rb
@@ -38,7 +38,31 @@ RSpec.describe TeamUsersController, type: :controller do
   end
 
   describe "GET #destroy" do
+    context "Team owner" do
+      before(:each) do
+        @team = create(:team, user: @current_user)
+        @guest_user = create(:user)
+        @team.users << @guest_user
+      end
 
+      it "returns http success" do
+        delete :destroy, params: { id: @guest_user.id, team_id: @team.id }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "Team not owner" do
+      before(:each) do
+        @team = create(:team)
+        @guest_user = create(:user)
+        @team.users << @guest_user
+      end
+
+      it "returns http forbidden" do
+        delete :destroy, params: { id: @guest_user.id, team_id: @team.id }
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
   end
 
 end
