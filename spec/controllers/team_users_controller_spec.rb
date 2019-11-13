109e9b88b7fd80736d710246ba7531a45d8357db
brandaoplaster <brandaoplaster@gmail.com>
2019-11-12 23:27:18 -0300

    Implemented test for the create method, in case the team has a leader and in the scenario where a team does have a leader

--- spec/controllers/team_users_controller_spec.rb
+++ spec/controllers/team_users_controller_spec.rb
@@ -12,7 +12,29 @@ RSpec.describe TeamUsersController, type: :controller do
   end
 
   describe "GET #crete" do
+    context "Team owner" do
+      before(:each) do
+        @team = create(:team, user: @current_user)
+        @guest_user = create(:user)
+      end
 
+      it "returns http success" do
+        post :create, params: { team_user: { user_id: @guest_user.id, team_id: @team.id } }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "Team not owner" do
+      before(:each) do
+        @team = create(:team)
+        @guest_user = create(:user)
+      end
+
+      it "returns http forbidden" do
+        post :create, params: { team_user: { user_id: @guest_user.id, team_id: @team.id } }
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
   end
 
   describe "GET #destroy" do
