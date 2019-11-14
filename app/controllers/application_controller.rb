66f0a6ffb1d4333a9c93057ff1c2227e5c0ceab0
brandaoplaster <brandaoplaster@gmail.com>
2019-11-13 23:14:18 -0300

    Included in application controller an exception to return a message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::Base
 
   include CanCan::ControllerAdditions
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+    end
+  end
+
 end
