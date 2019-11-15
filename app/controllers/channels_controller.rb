c7ebcd152dbb928c4c62b0fa7a2b0510b51dbe6e
brandaoplaster <brandaoplaster@gmail.com>
2019-11-15 19:22:24 -0300

    Create method implemented in controller channels, but only authorized users can create new channel

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,6 +2,16 @@ class ChannelsController < ApplicationController
   before_action :set_channel, only: [:destroy, :show]
 
   def create
+    @channel = Channel.new(channel_params)
+    authorize! :create, @channel
+
+    respond_to do |format|
+      if @channel.save
+        format.json { render :show, status: :created }
+      else
+        format.json { render json: @channel.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
