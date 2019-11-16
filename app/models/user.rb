3e7198b362dffd189f104b765fb30b38f77f13cc
brandaoplaster <brandaoplaster@gmail.com>
2019-11-16 15:34:05 -0300

    Adding in model user relationship with team_users and member_teams

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,7 @@ class User < ApplicationRecord
   has_many :teams
   has_many :messages
   has_many :talks, dependent: :destroy
+  has_many :team_users, dependent: :destroy
+  has_many :member_teams, through: :team_users, :source => :team
 
 end
