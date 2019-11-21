6002653329f158ba554a2d80ef4a1d7d375af80c
brandaoplaster <brandaoplaster@gmail.com>
2019-11-20 23:32:38 -0300

    Updated set_talk method to make sure you dont have a conversation create it

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -8,6 +8,15 @@ class TalksController < ApplicationController
   private
 
   def set_talk
-    @talk = Talk.find_by(user_one_id: [params[:id], current_user.id], user_two_id: [params[:id], current_user.id], team: params[:team_id])
+    @talk = Talk.find_by(user_one_id: [params[:id], current_user.id], user_two_id: [params[id]], team: params[:team_id])
+
+    unless @talk
+      @team = Team.find(params[:team_id])
+      @user = User.find(params[id])
+
+      if @tem.my_users.include? current_user and @team_my_users.include? @user
+        @talk = Talk.create(user_one: current_user, user_two: @user, team: @team)
+      end
+    end
   end
 end
